703ae87bedd3374a06be787dec203054930f537d
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-09 22:07:09 -0300

    tercer commit: punto 3 y 5

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -5,6 +5,7 @@ Random rand = new Random();
 string [] descrip = { "Prueba del programa", "Crear proyecto", "Cargar Datos", "Subir archivos", "Mostrar proyecto", "Generar aleatorios"};
 
 List<Tarea> TareasPendientes = new List<Tarea>();
+List<Tarea> TareasRealizadas = new List<Tarea>();
 
 Console.WriteLine("Ingrese el numero de tareas:");
 string input = Console.ReadLine();
@@ -19,9 +20,38 @@ for (int i = 0; i < N; i++)
     TareasPendientes.Add(nuevaTarea);
 }
 
-foreach (Tarea informacion in TareasPendientes)
+mostrarListas(TareasPendientes, "pendientes");
+mostrarListas(TareasRealizadas, "realizadas");
+
+Console.WriteLine("Ingrese el ID de la tarea Realizada: ");
+input = Console.ReadLine();
+esValido = int.TryParse(input, out int tRealizadaID);
+agregarRealizada(TareasPendientes, TareasRealizadas, tRealizadaID);
+
+mostrarListas(TareasPendientes, "pendientes");
+mostrarListas(TareasRealizadas, "realizadas");
+
+
+void agregarRealizada(List<Tarea> tareas1, List<Tarea> tareas2, int id)
 {
-    Console.WriteLine("\nTarea " + informacion.TareaID);
-    Console.WriteLine("Descripcion: " + informacion.Descripcion);
-    Console.WriteLine("Duracion: " + informacion.Duracion);
+    for (int i = 0; i < N; i++)
+    {
+        if (tareas1[i].TareaID == id)
+        {
+            Tarea TareaMovida = tareas1[i];
+            tareas1.Remove(TareaMovida);
+            tareas2.Add(TareaMovida);
+        }
+    }
+} 
+
+void mostrarListas(List<Tarea> tareas1, string tipo)
+{
+    Console.WriteLine("\nTareas " + tipo);
+    foreach (Tarea informacion in tareas1)
+    {
+        Console.WriteLine("\nTarea " + informacion.TareaID);
+        Console.WriteLine("Descripcion: " + informacion.Descripcion);
+        Console.WriteLine("Duracion: " + informacion.Duracion);
+    }
 }
