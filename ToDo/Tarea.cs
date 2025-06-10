ac5c8a854190a334a9266225d83896928421a031
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-09 21:32:23 -0300

    primer commit: punto 1

---
+++ ToDo/Tarea.cs
@@ -0,0 +1,37 @@
+namespace ToDo
+{
+    public class Tarea
+    {
+        private int tareaID;
+        private string descripcion;
+        private int duracion;
+
+        public int TareaID
+        {
+            get => tareaID;
+            set => tareaID = value;
+        }
+        public string Descripcion
+        {
+            get => descripcion;
+            set => descripcion = value;
+        }
+        public int Duracion
+        {
+            get => duracion;
+            set
+            {
+                if (value < 10 || value > 100)
+                {
+                    duracion = 0;
+                }
+                else
+                {
+                    duracion = value;
+                }
+            }
+        }
+        
+        
+    }
+}
