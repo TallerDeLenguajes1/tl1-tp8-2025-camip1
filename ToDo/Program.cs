5b351972768089142abb7a37637a7d8d7d4ff895
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-09 21:39:11 -0300

    segundo commit: punto 2

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,1 +1,27 @@
 ﻿using System.Linq.Expressions;
+using ToDo;
+
+Random rand = new Random();
+string [] descrip = { "Prueba del programa", "Crear proyecto", "Cargar Datos", "Subir archivos", "Mostrar proyecto", "Generar aleatorios"};
+
+List<Tarea> TareasPendientes = new List<Tarea>();
+
+Console.WriteLine("Ingrese el numero de tareas:");
+string input = Console.ReadLine();
+bool esValido = int.TryParse(input, out int N);
+
+for (int i = 0; i < N; i++)
+{
+    Tarea nuevaTarea = new Tarea();
+    nuevaTarea.TareaID = rand.Next(100);
+    nuevaTarea.Descripcion = descrip[rand.Next(6)];
+    nuevaTarea.Duracion = rand.Next(10, 100);
+    TareasPendientes.Add(nuevaTarea);
+}
+
+foreach (Tarea informacion in TareasPendientes)
+{
+    Console.WriteLine("\nTarea " + informacion.TareaID);
+    Console.WriteLine("Descripcion: " + informacion.Descripcion);
+    Console.WriteLine("Duracion: " + informacion.Duracion);
+}
